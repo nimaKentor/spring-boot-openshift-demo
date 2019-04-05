c62f104cc190305bf45ff164a40740be654735a9
nimaKentor <nima.mazdak@soprasteria.com>
2019-04-05 10:20:14 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootopenshiftdemo/SpringBootOpenshiftDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootopenshiftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenshiftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpenshiftDemoApplication.class, args);
+	}
+
+}
